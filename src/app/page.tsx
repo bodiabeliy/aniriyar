caf2b10f234af539ed3a9158bd8669d2f01e87cf
bodiabeliy <bboda2106@gmail.com>
2024-10-30 14:22:04 +0200

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import About from "@/components/About/About";
 import { Form } from "@/components/Form";
 import SpeakersAndSponsors from "@/components/SpeakersAndSponsors/SpeakersAndSponsors";
 import Banner from "@/components/banner/Banner";
-import Countdown from "@/widgets/Countdown/Countdown";
 
 export default function Home() {
   return (
